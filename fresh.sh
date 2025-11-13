6132242fbd4e575ea5b6750765dded3f099afb41
Thomas Navarro <thomas@smartfire.pro>
2025-11-13 11:23:04 +0100

    Correction de la commande de suppression et mise à jour du lien symbolique pour .zshrc dans fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -16,8 +16,8 @@ if test ! $(which brew); then
 fi
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -sw $HOME/.dotfiles/.zshrc $HOME/.zshrc
+rm -f $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Update Homebrew recipes
 brew update
